dce19507f0ac3ef12bb9575cb73519962f81670b
Shoaib <145601395+XSHOAIB@users.noreply.github.com>
2026-08-06 07:07:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+  base: '/Shoaib-Portfolio-V2/', // <-- Add this line right here  plugins: [react()],
   server: {
     host: true,
     port: 5173
